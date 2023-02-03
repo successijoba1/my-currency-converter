300d004d1a0ad00888833c0aebdd76779da3a828
successijoba1 <122977363+successijoba1@users.noreply.github.com>
2023-02-03 17:45:55 +0100

    first commit

---
+++ converter.js
@@ -0,0 +1,21 @@
+// var button = document.getElementById('button');
+function convert() {
+    var insert = document.getElementById('insert');
+    var result = document.getElementById('output');
+    var sum = insert.value/743
+        result.innerHTML = sum
+}
+// insert.addEventListener('keydown',()=>{
+//     var sum = insert.value/700
+//     result.innerHTML = sum
+// })// var button = document.getElementById('button');
+function converto() {
+    var insert = document.getElementById('insert');
+    var result = document.getElementById('output');
+    var sum = insert.value * 743
+        result.innerHTML = sum
+}
+// insert.addEventListener('keydown',()=>{
+//     var sum = insert.value/700
+//     result.innerHTML = sum
+// })
